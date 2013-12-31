06847a056999e992db0c37910c7cb0e098c36c95
Young <littlehaker@gmail.com>
2013-12-31 17:23:07 +0800

    indent fix

--- index.js
+++ index.js
@@ -6,29 +6,29 @@ var vm = require('vm')
 
 function coEval(cmd, context, filename, callback) {
    try {
-		 var result = eval(cmd);
+     var result = eval(cmd);
      callback(null, result);
    } catch(e) {
      context.___CALLBACK___ = callback;
-		 if (!context.___CO___) {
-			 context.___CO___ = require('co');
-		 }
-		 // Wrap with co
+     if (!context.___CO___) {
+       context.___CO___ = require('co');
+     }
+     // Wrap with co
      var cmd = '___CO___(function*(){___CALLBACK___(null, ' + cmd + ')})()';
-		 var script = vm.createScript(cmd);
-		 script.runInNewContext(context);
+     var script = vm.createScript(cmd);
+     script.runInNewContext(context);
    }
 };
 
 function start() {
-	var repl = REPL.start({
-		eval: coEval
-	});
-	require('repl.history')(repl, process.env.HOME + '/.node_history');
+  var repl = REPL.start({
+    eval: coEval
+  });
+  require('repl.history')(repl, process.env.HOME + '/.node_history');
 }
 
 if (!module.parent) {
-	start();
+  start();
 } else {
-	module.exports.start = start
+  module.exports.start = start
 }
